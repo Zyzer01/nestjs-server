c2b4ba010c3facda905fc298c239807279d00294
David Onifade <davidicfola@gmail.com>
2024-02-16 00:52:42 +0000

    update 6 files

--- src/recipe/dto/recipe.dto.ts
+++ src/recipe/dto/recipe.dto.ts
@@ -1,14 +1,28 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+  MinLength,
+  ValidateNested,
+} from 'class-validator';
+
 export class RecipeDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
   description: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => IngredientDto)
   ingredients: IngredientDto[];
 }
-
-export class IngredientDto {
-  name: string;
-  unit: Unit;
-  quantity: number;
-}
-
 export enum Unit {
   MILILITERS = 'mililiters',
   LITERS = 'LITERS',
@@ -17,4 +31,19 @@ export enum Unit {
   SPOONS = 'spoons',
   CUPS = 'cups',
   PIECES = 'pieces',
+}
+
+export class IngredientDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEnum(Unit)
+  unit: Unit;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  quantity: number;
 }
